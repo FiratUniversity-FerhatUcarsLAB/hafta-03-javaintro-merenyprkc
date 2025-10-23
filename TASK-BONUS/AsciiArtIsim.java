2492f71542355a358b043a1308ae0e077e950200
Muhammed Eren Yaprakcı <131323341+merenyprkc@users.noreply.github.com>
2025-10-23 09:56:31 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class AsciiArtIsim {
+        public static void main(String[] args) {
+        System.out.println("EEEEEEE  RRRRRR    EEEEEEE  NN    NN");
+        System.out.println("EE       RR   RR   EE       NNN   NN");
+        System.out.println("EEEE     RRRRR     EEEEE    NN NN NN");
+        System.out.println("EE       RR  RR    EE       NN   NNN");
+        System.out.println("EEEEEEE  RR   RR   EEEEEEE  NN    NN");
+    }
+}
