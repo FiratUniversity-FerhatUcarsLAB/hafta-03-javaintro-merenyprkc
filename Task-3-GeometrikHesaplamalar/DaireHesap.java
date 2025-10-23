68a454beaf0b1c053e28991955f70bea6df8932b
Muhammed Eren Yaprakcı <131323341+merenyprkc@users.noreply.github.com>
2025-10-23 09:14:07 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,13 @@
+public class DaireHesap {
+    public static void main(String[] args) {
+        double r = 5.5;
+        double pi = 3.14159;
+
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+        System.out.println("Yarıçap: " + r);
+        System.out.println("Alanı: " + alan);
+        System.out.println("Çevresi: " + cevre);
+    }
+}
