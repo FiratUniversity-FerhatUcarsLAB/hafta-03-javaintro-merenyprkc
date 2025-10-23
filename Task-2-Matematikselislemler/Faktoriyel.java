675ff5473ec199dc5a25bb8b2e617d1db4c9b33b
Muhammed Eren Yaprakcı <131323341+merenyprkc@users.noreply.github.com>
2025-10-23 09:13:33 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,13 @@
+public class Faktoriyel {
+    public static void main(String[] args) {
+        System.out.println("5! Hesaplama Adımları:");
+        int faktoriyel = 1*2;
+        System.out.println("  2!= " + faktoriyel);
+        faktoriyel = faktoriyel*3;
+        System.out.println("  3!= " + faktoriyel);
+        faktoriyel = faktoriyel*4;
+        System.out.println("  4!= " + faktoriyel);
+        faktoriyel = faktoriyel*5;
+        System.out.println("  5!= " + faktoriyel);
+    }
+}
