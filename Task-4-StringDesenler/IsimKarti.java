59b20f79bdb33a1737d34e1f737d24801f168f9c
Muhammed Eren Yaprakcı <131323341+merenyprkc@users.noreply.github.com>
2025-10-23 09:30:48 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Ahmet";
+        String soyad = "Yilmaz";
+        String ogrNo = "220301010";
+        String bolum = "Yazilim Muh.";
+
+        System.out.println("=====================");
+        System.out.println("  OGRENCI BILGILERI  ");
+        System.out.println("=====================");
+        System.out.println("Ad:     " + ad);
+        System.out.println("Soyad:  " + soyad);
+        System.out.println("No:     " + ogrNo);
+        System.out.println("Bolum:  " + bolum);
+        System.out.println("=====================");
+    }
+}
