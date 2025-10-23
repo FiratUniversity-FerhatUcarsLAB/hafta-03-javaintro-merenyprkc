5cc0a43a5942ff1b124c7455cdc1bee6575ba588
Muhammed Eren Yaprakcı <131323341+merenyprkc@users.noreply.github.com>
2025-10-23 09:51:46 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,8 @@
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+        int kilo = 65;
+        double boy = 1.75;
+        double bmi = kilo / (boy * boy);
+        System.out.println("Vücut Kitle İndeksi: " + bmi);
+    }
+}
