461020c177576fcfc158a364a3c4dd934d1e340b
Muhammed Eren Yaprakcı <131323341+merenyprkc@users.noreply.github.com>
2025-10-23 09:43:49 +0300

    Create multiplication table for the number 2
    This program prints the multiplication table for the number 2.

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,19 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        int sayi = 2;
+        System.out.println(sayi + "'nın Çarpım Tablosu");
+        System.out.println("---------------------");
+        System.out.println("Çarpan       Sonuç");
+        System.out.println("---------------------");
+        System.out.println("1            " + (1 * sayi));
+        System.out.println("2            " + (2 * sayi));
+        System.out.println("3            " + (3 * sayi));
+        System.out.println("4            " + (4 * sayi));
+        System.out.println("5            " + (5 * sayi));
+        System.out.println("6            " + (6 * sayi));
+        System.out.println("7            " + (7 * sayi));
+        System.out.println("8            " + (8 * sayi));
+        System.out.println("9            " + (9 * sayi));
+        System.out.println("10           " + (10 * sayi));
+    }
+}
